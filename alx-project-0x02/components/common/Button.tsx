17130faf6a2a75a5d92982cb51e899795e3a7913
Peter Adjao <pa376499@gmail.com>
2025-06-08 00:32:01 +0000

    feat: add reusable Button component with size and shape props

---
+++ alx-project-0x02/components/common/Button.tsx
@@ -0,0 +1,25 @@
+// components/common/Button.tsx
+import React from "react";
+import { ButtonProps } from "@/interfaces";
+
+const sizeClasses = {
+  small: "px-2 py-1 text-sm",
+  medium: "px-4 py-2 text-base",
+  large: "px-6 py-3 text-lg",
+};
+
+export default function Button({
+  label,
+  size = "medium",
+  shape = "rounded-md",
+  onClick,
+}: ButtonProps) {
+  return (
+    <button
+      className={`bg-blue-500 text-white ${sizeClasses[size]} ${shape} hover:bg-blue-600 transition`}
+      onClick={onClick}
+    >
+      {label}
+    </button>
+  );
+}
--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -1,3 +1,4 @@
+import Button from "@/components/common/Button";
 import Header from "../components/layout/Header";
 
 export default function AboutPage() {
@@ -6,6 +7,11 @@ export default function AboutPage() {
     <Header />{/*render the header */}
     <main className="min-h-screen flex items-center justify-center bg-blue-100 text-black">
       <h1 className="text-3xl font-bold">About Us</h1>
+
+      {/* Three buttons with different sizes and shapes */}
+        <Button label="Small Rounded" size="small" shape="rounded-sm" />
+        <Button label="Medium Pill" size="medium" shape="rounded-full" />
+        <Button label="Large Rounded" size="large" shape="rounded-md" />
     </main>
     </>
   );
